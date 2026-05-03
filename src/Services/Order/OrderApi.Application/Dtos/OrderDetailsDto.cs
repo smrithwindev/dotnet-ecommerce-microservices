44ccab2c2f41e582c8ca0c6778ecae0c6f54a9b5
smrithwindev <rithwindev2002@gmail.com>
2026-05-03 14:49:32 +0530

    feat(order-application): introduce OrderDetailsDto

---
+++ src/Services/Order/OrderApi.Application/Dtos/OrderDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.Dtos
+{
+    public record OrderDetailsDto(
+     [Required] int OrderId,
+     [Required] int ProductId,
+     [Required] int Client,
+     [Required, EmailAddress] string Email,
+     [Required] string TelephoneNumber,
+     [Required] string ProductName,
+     [Required] int PurchaseQuantity,
+     [Required, DataType(DataType.Currency)] decimal UnitPrice,
+     [Required, DataType(DataType.Currency)] decimal TotalPrice,
+     [Required] DateTime OrderedDate
+    );
+}
