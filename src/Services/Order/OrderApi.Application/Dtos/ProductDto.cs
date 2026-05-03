d207ef4771ca8b199a5ef61e84d31266cc628af5
smrithwindev <rithwindev2002@gmail.com>
2026-05-03 14:20:57 +0530

    feat(order-application): introduce ProductDto for product service contract

---
+++ src/Services/Order/OrderApi.Application/Dtos/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.Dtos
+{
+    public record ProductDto
+    (
+      int Id,
+      [Required] string Name,
+      [Required, Range(1, int.MaxValue)] int Quantity,
+      [Required, DataType(DataType.Currency)] decimal Price
+    );
+}
