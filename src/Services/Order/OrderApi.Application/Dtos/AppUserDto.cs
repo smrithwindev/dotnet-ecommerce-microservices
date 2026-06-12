d3509b40f63709f5215aa24128024c576c2a5df0
smrithwindev <rithwindev2002@gmail.com>
2026-06-12 23:52:36 +0530

    feat(order-application): add DTOs for order details and user integration

---
+++ src/Services/Order/OrderApi.Application/Dtos/AppUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.Dtos
+{
+    public record AppUserDto(
+        int Id,
+        [Required] string UserName, //or simply name, as the username is the name of the client
+        [Required] string TelephoneNumber,
+        [Required] string Address,
+        [Required, EmailAddress] string Email,
+        [Required] string Password,
+        [Required] string Role
+    );
+
+}
