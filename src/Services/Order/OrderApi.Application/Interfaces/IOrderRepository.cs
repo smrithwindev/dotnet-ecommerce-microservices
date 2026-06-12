0267fba7eb420642fc97be8066736e418e4aa61a
smrithwindev <rithwindev2002@gmail.com>
2026-06-12 23:53:26 +0530

    feat(order-application): add order repository abstraction

---
+++ src/Services/Order/OrderApi.Application/Interfaces/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Core.Interfaces;
+using OrderApi.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace OrderApi.Application.Interfaces
+{
+    public interface IOrderRepository : IGenericInterface<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersAsync(Expression<Func<Order, bool>> predicate);
+    }
+}
