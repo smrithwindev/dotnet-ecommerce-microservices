252982c93b985a6479a1cb6508c3cf6d053b57fb
smrithwindev <rithwindev2002@gmail.com>
2026-06-21 23:29:22 +0530

    feat(order-presentation): configure application startup and settings

--- src/Services/Order/OrderApi.Presentation/Program.cs
+++ src/Services/Order/OrderApi.Presentation/Program.cs
@@ -1,24 +1,20 @@
+using OrderApi.Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureService(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseInfrastructurePolicy();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
 
 
