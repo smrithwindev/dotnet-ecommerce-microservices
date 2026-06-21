6014418cec20795548add3c7d40696f7a8ffcd9f
smrithwindev <rithwindev2002@gmail.com>
2026-06-21 23:19:41 +0530

    feat(order-infrastructure): configure service registration and middleware policies

---
+++ src/Services/Order/OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.Web.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repositories;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            // Add Database Connectivity
+            // Add authentication scheme
+            SharedServiceContainer.AddSharedServices<OrderDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            // Create Dependency Injection
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            // Register middleware such as:
+            // Global Exception -> handle external errors
+            // ListenToApiGateway Only -> block all outsiders calls
+            SharedServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
