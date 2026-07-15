aa2483f98fd811490102a8a4d350dfa358227992
smrithwindev <rithwindev2002@gmail.com>
2026-07-15 14:18:51 +0530

    chore(order): register application services and API gateway configuration

--- src/Services/Order/OrderApi.Presentation/Program.cs
+++ src/Services/Order/OrderApi.Presentation/Program.cs
@@ -1,4 +1,5 @@
 using OrderApi.Infrastructure.DependencyInjection;
+using OrderApi.Application.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureService(builder.Configuration);
+builder.Services.AddApplicationService(builder.Configuration);
 
 var app = builder.Build();
 
