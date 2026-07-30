53c3ffd3f10f022692fcbe95d6ecfc9bb9019a02
smrithwindev <rithwindev2002@gmail.com>
2026-07-30 23:47:32 +0530

    feat(authentication-application): add user repository abstraction

---
+++ src/Services/Authentication/AuthenticationApi.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using AuthenticationApi.Application.Dtos;
+using AuthenticationApi.Domain.Entities;
+using BuildingBlocks.Core.Responses;
+
+
+namespace AuthenticationApi.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<AppUser> GetUserByEmail(string email);
+        Task<GetUserDto> GetUser(int userId);
+        Task<Response> Register(AppUserDto appUserDto);
+        Task<Response> Login(LoginDto loginDto);
+    }
+}
