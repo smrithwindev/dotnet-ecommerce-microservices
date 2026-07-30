bf76ea0b7c1b6e860fbbf1dd56c2aef11e440b93
smrithwindev <rithwindev2002@gmail.com>
2026-07-30 23:48:54 +0530

    feat(authentication-infrastructure): add AuthenticationDbContext

---
+++ src/Services/Authentication/AuthenticationApi.Infrastructure/Data/AuthenticationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AuthenticationApi.Domain.Entities;
+using Microsoft.AspNetCore.Routing.Patterns;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationApi.Infrastructure.Data
+{
+    public class AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+        }
+}
