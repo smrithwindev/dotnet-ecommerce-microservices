f6da38b5d18dcf059795bb8ac500ea652c76ec9e
smrithwindev <rithwindev2002@gmail.com>
2026-09-09 23:12:05 +0530

    feat(buildingblocks-web): add feature permission authorization

---
+++ src/BuildingBlocks/BuildingBlocks.Web/Authorization/FeaturePermissionHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BuildingBlocks.Web.Authorization
+{
+    public class FeaturePermissionHandler : AuthorizationHandler<FeaturePermissionRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,FeaturePermissionRequirement requirement)
+        {
+            var permissionClaims = context.User.FindAll("Permissions");
+            foreach (var claim in permissionClaims)
+            {
+                var parts = claim.Value.Split(':');
+
+                if (parts.Length != 2)
+                    continue;
+
+                var feature = parts[0];
+
+                if (feature != requirement.Feature)
+                    continue;
+
+                if (int.TryParse(parts[1], out int userLevel) &&
+                    userLevel >= (int)requirement.RequiredLevel)
+                {
+                    context.Succeed(requirement);
+                    break;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Web/Authorization/FeaturePermissionRequirement.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Core.Authorization;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BuildingBlocks.Web.Authorization
+{
+    public class FeaturePermissionRequirement : IAuthorizationRequirement
+    {
+        public string Feature { get; }
+
+        public PermissionLevel RequiredLevel { get; }
+
+        public FeaturePermissionRequirement(
+            string feature,
+            PermissionLevel requiredLevel)
+        {
+            Feature = feature;
+            RequiredLevel = requiredLevel;
+        }
+    }
+}
