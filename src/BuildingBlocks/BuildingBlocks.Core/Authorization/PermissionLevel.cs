5750de095ddb3f4f8d7f74c5e64d42cb7f1d9402
smrithwindev <rithwindev2002@gmail.com>
2026-09-09 23:11:01 +0530

    feat(buildingblocks-core): add permission level definitions

---
+++ src/BuildingBlocks/BuildingBlocks.Core/Authorization/PermissionLevel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BuildingBlocks.Core.Authorization
+{
+    public enum PermissionLevel
+    {
+        None = 0,
+        Read = 1,
+        Write = 2,
+        FullAccess = 3
+    }
+}
