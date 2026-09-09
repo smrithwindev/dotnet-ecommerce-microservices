0de1eafcb208c79bafe45689a4b559975399465e
smrithwindev <rithwindev2002@gmail.com>
2026-09-09 23:16:51 +0530

    feat(order-presentation): enable authentication

--- src/Services/Order/OrderApi.Presentation/Program.cs
+++ src/Services/Order/OrderApi.Presentation/Program.cs
@@ -15,6 +15,7 @@ app.UseInfrastructurePolicy();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
